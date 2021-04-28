88cc3c8fe8dec9cd164d0889dd9019335b487c5e
LinkWasTaken <67402756+LiterallyLink@users.noreply.github.com>
2021-04-28 14:40:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+// dependancies
+
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
+
+// parsing the express package through cors
 app.use(cors());
 
+// starting a local server off of your machine using port 4000
 app.listen(4000, () => {
     console.log('Server Works !!! At port 4000');
 });
+
+// parsing the video request and downloading it to your /downlaod directory as an mp4 file
 
 app.get('/download', (req,res) => {
     let URL = req.query.URL;
