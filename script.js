a851f37b1f8e459e2f8d8af2ac0ce78d1a18ef3a
LinkWasTaken <67402756+LiterallyLink@users.noreply.github.com>
2021-04-28 14:41:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+// button variable and input variable
+
 let convertBtn = document.querySelector('.convert-button');
 let URLinput = document.querySelector('.URL-input');
 
+// adding a listener function to activate when the button is clicked
 convertBtn.addEventListener('click', () => {
+    // logging the url inputted
     console.log(`URL: ${URLinput.value}`);
+    //parsing the URL value to the sendURL() function below
     sendURL(URLinput.value);
 });
+
+// sending the url to the href location using the local host
 function sendURL(URL) {
     window.location.href = `http://localhost:4000/download?URL=${URL}`;
 }
